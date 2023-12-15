15533ebec17c797600aed3dea57c959afbc632b7
Ayush Aryan <ayusharyan1309@gmail.com>
2023-12-16 00:25:46 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:amazon_clone/common/widgets/bottom_bar.dart';
+import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
+import 'package:amazon_clone/features/auth/services/auth_service.dart';
+import 'package:amazon_clone/features/home/screens/home_screen.dart';
+import 'package:amazon_clone/provider/user_provider.dart';
+import 'package:amazon_clone/router.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'features/admin/screens/admin_screen.dart';
+
+void main() {
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(
+      create: (context) => UserProvider(),
+    ),
+  ], child: const MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // instance of AuthService called here.
+  final AuthService authService = AuthService();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    authService.getUserData(context);   // authService called
+  }
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Amazon Clone',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme:  const ColorScheme.light(
+            primary: GlobalVariables.secondaryColor,
+          ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black
+          )
+        )
+      ),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: Provider.of<UserProvider>(context).user.token.isNotEmpty
+          ? Provider.of<UserProvider>(context).user.type == 'user'
+          ? const BottomBar()
+          : const AdminScreen()
+          : const AuthScreen(),  // if token not empty -> go to homePage , else authPage
+    );
+  }
+}
